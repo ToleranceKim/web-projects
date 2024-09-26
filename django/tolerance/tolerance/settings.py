1f37f84a1457495352060fb68a65ba49ea9c4b22
Yong <kwanyou2@gmail.com>
2024-09-26 21:14:51 +0900

    Update django

--- django/tolerance/tolerance/settings.py
+++ django/tolerance/tolerance/settings.py
@@ -12,9 +12,20 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+import os, environ
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# reading .env file
+environ.Env.read_env(
+    env_file=os.path.join(BASE_DIR, '.env')
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
